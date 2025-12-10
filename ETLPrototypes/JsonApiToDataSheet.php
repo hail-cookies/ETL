bf8df3c852eefaf2dff8bedf68eb66855b81e956
hail-cookies <81558994+hail-cookies@users.noreply.github.com>
2025-12-10 15:12:43 +0100

    DEV JsonApiToDataSheet improved logging for duplicates matcher

--- ETLPrototypes/JsonApiToDataSheet.php
+++ ETLPrototypes/JsonApiToDataSheet.php
@@ -3,6 +3,7 @@ namespace axenox\ETL\ETLPrototypes;
 
 use axenox\ETL\Common\AbstractAPISchemaPrototype;
 use axenox\ETL\Common\AbstractETLPrototype;
+use axenox\ETL\Common\OpenAPI\OpenAPI3ObjectSchema;
 use axenox\ETL\Common\StepNote;
 use axenox\ETL\Common\Traits\BypassDataAuthorizationStepTrait;
 use axenox\ETL\Common\Traits\PreventDuplicatesStepTrait;
@@ -18,6 +19,7 @@ use axenox\ETL\Common\FlowStepLogBook;
 use exface\Core\CommonLogic\UxonObject;
 use exface\Core\DataTypes\ArrayDataType;
 use exface\Core\DataTypes\MessageTypeDataType;
+use exface\Core\Exceptions\DataSheets\DataMatcherError;
 use exface\Core\Exceptions\DataSheets\DataSheetInvalidValueError;
 use exface\Core\Exceptions\InvalidArgumentException;
 use exface\Core\Exceptions\RuntimeException;
@@ -249,9 +251,6 @@ class JsonApiToDataSheet extends AbstractAPISchemaPrototype
         $this->getCrudCounter()->addValueToCounter($fromSheet->countRows(), CrudCounter::COUNT_READS);
         
         $toSheet = $this->getToSheet($stepData, $fromSheet, $toObjectSchema, $logBook);
-        if (empty($this->getUpdateIfMatchingAttributeAliases()) && $toObjectSchema->isUpdateIfMatchingAttributes()) {
-            $this->setUpdateIfMatchingAttributes($toObjectSchema->getUpdateIfMatchingAttributeAliases());
-        }
         
         $logBook->addSection('Saving data');
         $lap = $profiler->start('Saving data');
@@ -506,12 +505,45 @@ class JsonApiToDataSheet extends AbstractAPISchemaPrototype
         if (null !== $checksUxon = $this->getToDataChecksUxon()) {
             $this->performDataChecks($toSheet, $checksUxon, 'to_data_checks', $stepData, $logbook);
         }
-        
+
+        // Extract match aliases from schema.
+        $matchAliasesFromSchema = [];
+        if (empty($this->getUpdateIfMatchingAttributeAliases())) {
+            $toObjectSchema = $this->getAPISchema($stepData)->getObjectSchema($toSheet->getMetaObject());
+            if($toObjectSchema->isUpdateIfMatchingAttributes()) {
+                $matchAliasesFromSchema = $toObjectSchema->getUpdateIfMatchingAttributeAliases();
+                $this->setUpdateIfMatchingAttributes($matchAliasesFromSchema);
+            }
+        }
+
         // Separate CREATEs from UPDATEs
         if (null !== $matcher = $this->getDuplicatesMatcher($toSheet, $logbook)) {
             $logbook->addSection('Finding existing data (duplicates)');
             $lap = $stepData->getProfiler()->start('Finding existing data (duplicates)');
-            $matchCollection = $matcher->getMatchesToUpdate($logbook);
+
+            if(!empty($matchAliasesFromSchema)) {
+                $hint = ' Refer to the property "' . OpenAPI3ObjectSchema::X_UPDATE_IF_MATCHING_ATTRIBUTES . '" in your OpenApi Definition' .
+                    ' for object "' . $toSheet->getMetaObject()->getAlias() . '".';
+                
+                $logbook->addLine('Found "' . OpenAPI3ObjectSchema::X_UPDATE_IF_MATCHING_ATTRIBUTES . 
+                    '" in schema. Will check for duplicates using these aliases ' . json_encode($matchAliasesFromSchema) . '.');
+            } else {
+                $hint = ' Refer to the property "update_if_matching_attributes" in your step definition.';
+            }
+
+            try {
+                $matchCollection = $matcher->getMatchesToUpdate($logbook);
+            } catch (DataMatcherError $e) {
+                throw new DataMatcherError(
+                    $matcher,
+                    $e->getMessage() . $hint,
+                    '7PNKJ51',
+                    null,
+                    $logbook,
+                    $e->getMatch()
+                );
+            }
+
             $lap->stop();
 
             if (! $matcher->hasMatches()) {
